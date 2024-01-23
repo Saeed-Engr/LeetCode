af1d111e2fcb196f702e460ea54346bd57a1e352
Muhammad Saeed <msaeed4771@gmail.com>
2024-01-23 22:02:51 +0500

    Time: 60 ms (59.20%), Space: 16.6 MB (54.40%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
         
         ans = [words[0]]
@@ -12,5 +12,25 @@ class Solution:
                 continue
             else:
                 ans.append(words[i])
+                
+        return ans"""
+
+class Solution:
+    def removeAnagrams(self, words):
+        # Initialize the answer list with the first word
+        ans = [words[0]]
+        
+        # Iterate over the words starting from the second one
+        for i in range(1, len(words)):
+            # Compare the sorted versions of the last word in ans and the current word
+            if sorted(ans[-1]) == sorted(words[i]):
+                # Skip adding the current word if it is an anagram of the last one in ans
+                continue
+            else:
+                # Add the current word to the answer list if it's not an anagram
+                ans.append(words[i])
+        
+        # Return the final list after removing consecutive anagrams
         return ans
-            
+
+
